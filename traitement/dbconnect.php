a613f91e49485da547b3ad99ecc8ff22e738d2fc
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-01-05 16:07:41 +0100

    connection a la base de donner creation select dinamique

--- traitement/dbconnect.php
+++ traitement/dbconnect.php
@@ -1,6 +1,7 @@
 <?php
+
 try {
-    $db =new PDO('mysql:host=localhost;dbname=cinema,'root','root');
-} catch (\Throwable $th) {
-    //throw $th;
+    $db = new PDO('mysql:host=localhost; dbname=cinema', 'mohamadi','mohamadi');
+} catch (exeception $e) {
+    die('Erreur'.$e->getMessage());
 }
