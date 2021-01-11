c44d230d42bf46efc7448760aa4eb4141e7ff3df
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-01-11 22:36:37 +0100

    recherche par prenom et par nom ok

--- traitement/filtre_client.php
+++ traitement/filtre_client.php
@@ -8,6 +8,7 @@ if (isset($_POST["nom"])) {
 
     $clientNoms = $db->prepare("SELECT * FROM fiche_personne WHERE nom LIKE $nom ORDER BY nom ASC");
     $clientPrenoms = $db->prepare("SELECT * FROM fiche_personne WHERE prenom LIKE $prenom ORDER BY nom ASC");
+    $clientPrenoms_noms = $db->prepare("SELECT * FROM fiche_personne WHERE nom LIKE $nom and  prenom LIKE $prenom");
 
     if ($_POST["nom"] != "" && $_POST["prenom"] == "" && $_POST["nomville"] == "" && $_POST["code_postale"] == "" && !isset($_POST["membre"])) {
         if ($clientNoms->execute()) {
@@ -17,6 +18,11 @@ if (isset($_POST["nom"])) {
     elseif($_POST["prenom"] != "" && $_POST["nom"] == "" && $_POST["nomville"] == "" && $_POST["code_postale"] == "" && !isset($_POST["membre"])) {
         if ($clientPrenoms->execute()) {
             $clientTrouver = $clientPrenoms->fetchAll(PDO::FETCH_OBJ);
+        }
+    }
+    elseif($_POST["prenom"] != "" && $_POST["nom"] != "" && $_POST["nomville"] == "" && $_POST["code_postale"] == "" && !isset($_POST["membre"])) {
+        if ($clientPrenoms_noms->execute()) {
+            $clientTrouver = $clientPrenoms_noms->fetchAll(PDO::FETCH_OBJ);
         }
     }
 }
