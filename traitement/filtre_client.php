cae5e852bc3d10f32e669172a7cbdcafa1e94aaa
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-01-11 22:17:52 +0100

    recherche par nom ok

--- traitement/filtre_client.php
+++ traitement/filtre_client.php
@@ -7,10 +7,16 @@ if (isset($_POST["nom"])) {
     $prenom = "'" . $_POST["prenom"] . "%'";
 
     $clientNoms = $db->prepare("SELECT * FROM fiche_personne WHERE nom LIKE $nom ORDER BY nom ASC");
+    $clientPrenoms = $db->prepare("SELECT * FROM fiche_personne WHERE prenom LIKE $prenom ORDER BY nom ASC");
 
     if ($_POST["nom"] != "" && $_POST["prenom"] == "" && $_POST["nomville"] == "" && $_POST["code_postale"] == "" && !isset($_POST["membre"])) {
         if ($clientNoms->execute()) {
             $clientTrouver = $clientNoms->fetchAll(PDO::FETCH_OBJ);
+        }
+    }
+    elseif($_POST["prenom"] != "" && $_POST["nom"] == "" && $_POST["nomville"] == "" && $_POST["code_postale"] == "" && !isset($_POST["membre"])) {
+        if ($clientPrenoms->execute()) {
+            $clientTrouver = $clientPrenoms->fetchAll(PDO::FETCH_OBJ);
         }
     }
 }
