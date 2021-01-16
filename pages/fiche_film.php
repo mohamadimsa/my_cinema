3f0e044c98d36d69a9f3c6a9759795df1e585f71
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-01-16 21:40:11 +0100

    desciption film ok mise en place image par default si l'image de l'api functionne pas

--- pages/fiche_film.php
+++ pages/fiche_film.php
@@ -7,7 +7,20 @@ require_once 'hautPage.php';
      <div class="row">FICHE FILM</div>
    <div class="row">
       <div class="col-3">
-     
+        <img classe="image_film" width="209px" src="<?php if(isset($image)){echo $image;} ?>" alt="image tu titre selectionner">
+      </div>
+      <div class="col">
+      <div class="desciption">
+         <h6><span>TITRE</span> &nbsp :&nbsp &nbsp <?= $details->titre?></h6>
+         <p>Genre &nbsp:&nbsp &nbsp <?= $details->nom?></p> 
+         <p>Distributeur &nbsp:&nbsp &nbsp <?= $production->nom?></p>
+         <p>Année de prod &nbsp:&nbsp &nbsp <?= $details->annee_prod?></p> 
+         <p>duréé du film &nbsp:&nbsp &nbsp <?= $details->duree_min?> min</p> 
+      </div>
+      <div class="resum">
+        <h6>Resumer &nbsp:</h6>
+        <p><?=$details->resum?></p>
+      </div>
       </div>
    </div>
 
--- traitement/fiche_film.php
+++ traitement/fiche_film.php
@@ -1,7 +1,27 @@
 <?php
-$recherche="man";
+require_once 'dbconnect.php';
+$data = "";
+if (isset($_GET["id"])) {
+    $id_cli = intval($_GET["id"]);
+
+$detailsfilm = $db->prepare("SELECT * from film left join genre on film.id_genre = genre.id_genre where film.id_film=:id_film ORDER BY genre.nom ");
+$detailsfilm->bindValue(":id_film",$id_cli,PDO::PARAM_INT);
+$distib = $db->prepare("SELECT nom FROM distrib where id_distrib=:distrib");
+
+if ($detailsfilm->execute()){
+    $details = $detailsfilm->fetch(PDO::FETCH_OBJ);
+    $prod = $details->id_distrib;
+    $prod = intval($prod);
+$distib->bindValue(":distrib",$prod,PDO::PARAM_INT);
+if ($distib->execute()) {
+    $production = $distib->fetch(PDO::FETCH_OBJ);
+}
+$recherche=$details->titre;
+$recherche=trim($recherche);
+$recherche= rtrim($recherche);
+$recherche= str_replace(" ","_",$recherche);
  
- $curl = curl_init();
+$curl = curl_init();
 $url = "http://www.omdbapi.com/?t=".$recherche."&apikey=26c135a9";
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
@@ -13,10 +33,15 @@ curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
     echo 'Erreur Curl : ' . curl_error($curl);
 
  }else{
-    
     $data = json_decode($data);
-    echo $data->Poster;
+    if (isset($data->Poster)) {
+        $image = $data->Poster; 
+    }
+    else{
+        $image = "../assets/default.jpg";
+    }
+     
 }
-
-
 curl_close($curl);
+}
+}
